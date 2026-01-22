dea205e831c5389b9893f93ab144d2c1b0a2acce
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-01-22 15:50:06 +0300

    feat: implement MailKit SMTP integration and Summernote rich text editor

---
+++ SignalRDine.WebUI/Controllers/MailController.cs
@@ -0,0 +1,43 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using SignalRDine.WebUI.Dtos.MailDtos;
+
+namespace SignalRDine.WebUI.Controllers
+{
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CreateMailDto createMailDto)
+        {
+            MimeMessage mimeMessage=new MimeMessage();
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("SignalR Rezervasyon", "sevilaygalatasaray5@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", createMailDto.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder=new BodyBuilder();
+            bodyBuilder.HtmlBody=createMailDto.Body;
+            mimeMessage.Body=bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = createMailDto.Subject;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+            smtpClient.Authenticate("sevilaygalatasaray5@gmail.com", "wworxavzlxgkwmqb");
+
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+
+
+            return RedirectToAction("Index","Category");
+        }
+    }
+}
