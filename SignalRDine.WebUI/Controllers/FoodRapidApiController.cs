b25be984fbaf03e6ac5b0ef533c7a4d7ba6c5772
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-01-23 21:03:06 +0300

    feat: integrate Tasty RapidAPI for world cuisine recipe listing

---
+++ SignalRDine.WebUI/Controllers/FoodRapidApiController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRDine.WebUI.Dtos.RapidApiDtos;
+
+namespace SignalRDine.WebUI.Controllers
+{
+    public class FoodRapidApiController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://tasty.p.rapidapi.com/recipes/list?from=0&size=60&tags=under_30_minutes"),
+                Headers =
+    {
+        { "x-rapidapi-key", "a549d544b1msh67e211c6ab180fdp1a3f5djsnc5817f75056b" },
+        { "x-rapidapi-host", "tasty.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var root =JsonConvert.DeserializeObject<RootTastyApi>(body);
+                var values = root.Results.ToList();
+                return View(values);
+
+            }
+        }
+    }
+}
